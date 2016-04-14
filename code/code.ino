7f4203feb0528a534c8894fbf5d47b7756aaf089
Artsiom Miksiuk <artsiom.miksiuk@sap.com>
2016-04-14 23:41:33 +0300

    Fourier transform implemented.

--- code/code.ino
+++ code/code.ino
@@ -1,53 +1,52 @@
 #include <SoftwareSerial.h>
+#include "fix_fft.h"
 
 #define COLS_NUMS 8
 #define ROWS_NUMS 8
 
 const int audioIn = A0;
 
-SoftwareSerial serial(12, 13);
+const int N = 128;
+const int LOG_2_N = 7;
+
+char im[N], re[N];
+
+//SoftwareSerial serial(A2, A3);
 
 void setup() {
-  serial.begin(9600);
+//  serial.begin(9600);
   DDRD = 0xFF; //D-port as output. For selecting row
-//  DDRB = B00000111; //Three last as outputs
+  DDRB = B00011111; //Three last as outputs
+
+  //for serial
+//  pinMode(A2, INPUT);
+//  pinMode(A3, OUTPUT);
 }
 
 void loop() {
-  int audioVal = analogRead(audioIn) >> 7; //decreasing resolution to 3 (10 - 7) bits
-  serial.println(audioVal);
+  //collecting data
+  for(int i = 0; i < N; i++) {
+    re[i] = analogRead(audioIn);
+    im[i] = 0;
+  }
+  //transform
+  fix_fft(re, im, LOG_2_N, 0);
 
-  PORTD = ~((1 << audioVal) - 1);
+  //we interested only in absolute values
+  for(int i = 0; i < 64; i++) {
+    re[i] =  sqrt(re[i] * re[i] + im[i] * im[i]);
+  }
 
-  delay(100);
-  
-//  for(int col = 0; col < COLS_NUMS; col++) {
-//    PORTB = col;
-//    for(int row = 0; row < ROWS_NUMS; row++) {
-//      PORTD = 1 << row;
-//      delay(100);
-//    }
-//  }
-}
+  //collapsing to 32 columns of our display, so displayed bandwidth is from 0 to 16kHz
+  for(int i = 0; i < 64; i+= 2) {
+    //TODO: investigate why output range after fft has maximum value 30
+    re[i] = map(re[i] + re[i+1], 0, 30, 0, 8); //calc avarage and remap value to 0 to 8 range.
+  }
 
-inline int toRange(int oldMin, int oldMax, int newMin, int newMax, int val) {
-  return ( (val - oldMin) / (oldMax - oldMin) ) * (newMax - newMin) + newMin;
-}
-
-unsigned int flp2(int x) {
-  x = x | (x >> 1);
-  x = x | (x >> 2);
-  x = x | (x >> 4);
-  x = x | (x >> 8);
-  return x - (x >> 1);
-}
-
-unsigned int clp2(int x) {
-  x = x - 1;
-  x = x | (x >> 1);
-  x = x | (x >> 2);
-  x = x | (x >> 4);
-  x = x | (x >> 8);
-  return x + 1;
+  for(int col = 0; col < 8; col++) {
+    PORTB = col;
+    PORTD = ~((1 << re[col]) - 1); //output col val
+  }
+  delay(15);
 }
 
